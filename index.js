1da7e089ea1b640a8b3c4b5d1f6ea0894276acaf
SQLAdmin <abuyeahia24@gmail.com>
2023-06-12 04:48:27 +0600

    get payments for show data of payment

--- index.js
+++ index.js
@@ -261,6 +261,11 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/payments', async(req, res)=>{
+            const result = await paymentsCollection.find().toArray();
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
